922e98809be571dfb0163727417f32d9f463896b
余俊卿 <yujunqing@meizu.com>
2015-01-22 12:09:49 +0800

    init

---
+++ core/src/com/meizu/taskmanager/policy/ManagerService.java
@@ -0,0 +1,10 @@
+package com.meizu.taskmanager.policy;
+
+public class ManagerService {
+    public static void kill(){
+
+    }
+    public static void getImage(){
+
+    }
+}
---
+++ desktop/src/com/meizu/taskmanager/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.meizu.taskmanager.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.meizu.taskmanager.TaskManager;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TaskManager(), config);
+	}
+}
